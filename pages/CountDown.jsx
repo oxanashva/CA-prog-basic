e2b64292f97d88e0d0e76b59d3f2e375c3b61559
oxanashva <oxanashva@gmail.com>
2025-08-21 19:11:15 +0300

    fix: Add comments and organize code in CountDown component

--- pages/CountDown.jsx
+++ pages/CountDown.jsx
@@ -3,45 +3,55 @@ import { utilService } from "../services/util.service.js"
 const { useState, useEffect, useRef } = React
 
 export function CountDown({startFrom, toTime, onDone, onChangeBackground}) {
-    const initialTimeValue = toTime ? toTime - Date.now() : startFrom * 1000 //milliseconds
-    const [time, setTime] = useState(initialTimeValue)
+    // `countdown`: current countdown time in milliseconds
+    // If `toTime` is provided, calculate remaining time by subtracting current timestamp
+    // Otherwise, initialize with `startFrom` converted from seconds to milliseconds
+    const [countdown, setCountdown] = useState(toTime ? toTime - Date.now() : startFrom * 1000)
+
     const intervalId = useRef(null)
     const elTime = useRef(null)
     
     useEffect(() => {
-        if (time <= 0) {
-            clearInterval(intervalId.current)
-            toTime ? onDone('Its Time!', elTime.current) : onDone('Done!', elTime.current)
+        if (countdown <= 0) {
+            handleDone()
             return
         }
 
         intervalId.current = setInterval(() => {
-            setTime(prevTime => {
+            setCountdown(prevTime => {
                 const newTime = prevTime - 1000
+
                 if (newTime <= 0) {
-                    clearInterval(intervalId.current)
-                    toTime ? onDone('Its Time!', elTime.current) : onDone('Done!', elTime.current)
+                    handleDone()
                     return 0
                 }
+
                 return newTime
             })
         }, 1000)
 
         return () => clearInterval(intervalId.current)
     }, [])
-    
-    const displayTime = toTime ? utilService.formatTime(Math.floor(time / 1000)) : Math.floor(time / 1000)
-    const classRed = time / 1000 < 6 ? 'red' : ''
 
     useEffect(() => {
         onChangeBackground({backgroundColor: 'lightblue', color: '#0a2694'})
     }, [])
+    
+    function handleDone() {
+        clearInterval(intervalId.current)
+        if (toTime) onDone('Its Time!', elTime.current)
+        else onDone('Done!', elTime.current)
+    }
+
+    const displayTime = toTime ? utilService.formatTime(Math.floor(countdown / 1000)) : Math.floor(countdown / 1000)
+
+    const className = (countdown / 1000) < 6 ? 'red' : ''
 
     return (
         <section className="count-down">
             <div className="box box1">
                 <div className="box box2">
-                    <p ref={elTime} className={`time ${classRed}`}>{displayTime}</p>
+                    <p ref={elTime} className={`time ${className}`}>{displayTime}</p>
                 </div>
             </div>
         </section>
